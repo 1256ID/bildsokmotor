63b63faf4db2aeb409a2b4b7df6f9d0cdf15e91a
TheAbsintheDuck <gabriel-carlsson@hotmail.com>
2023-02-14 14:53:47 +0100

    Display images

--- node.js
+++ node.js
@@ -1,21 +1,31 @@
 let form = document.querySelector('form');
-let imageList = document.querySelector('Pictures');
+let imageList = document.querySelector('#pictures');
+let gallery = document.querySelector('#navButtons');
 
 form.onsubmit = async event => {
-    event.precentDeafault();
+    event.preventDefault();
 
     let search = form.search.value;
     let color = form.color.value;
+    let pageNr = 1;
 
-    let url = "https://pixabay.com/api/?key=33470155-a1510963a99de7f2888f9d89f&q=" + color + "+" + search + "&image_type=photo";
+    let url = "https://pixabay.com/api/?key=33470155-a1510963a99de7f2888f9d89f&q=" + color + "+" + search + "&per_page=10" + "&page=" + pageNr;
 
     let response = await fetch(url);
     let json = await response.json();
 
 
+    for (let hit of json.hits) {
 
+        let img = document.createElement('img');
+        img.src = hit.previewURL;
+        let li = document.createElement('li');
+
+        li.appendChild(img);
+        imageList.appendChild(li);
+    }
 
     form.search.value = "";
     form.color.value = "";
 
- };
+};
