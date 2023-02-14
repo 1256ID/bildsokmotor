c4a764941cac42f8ecb1e64f2209839a972e8b99
1256ID <112385041+1256ID@users.noreply.github.com>
2023-02-14 11:16:08 +0100

    input id

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ let imageList = document.querySelector('#pictures');
 form.onsubmit = async event => {
     event.precentDeafault();
 
-    let search = form.search.value;
+    let search = form.input.value;
     let color = form.color.value;
 
     let url = "https://pixabay.com/api/?key=33470155-a1510963a99de7f2888f9d89f&q=" + color +"+" + search +"&image_type=photo";
