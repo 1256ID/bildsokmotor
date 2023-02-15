582977289f1f5d91c99dd4abd1adf3059e088745
1256ID <112385041+1256ID@users.noreply.github.com>
2023-02-15 13:58:00 +0100

    editing format

--- main.js
+++ main.js
@@ -7,16 +7,32 @@ let nextPage = false;
 form.onsubmit = async event => {
     event.preventDefault();
 
-    let search = form.search.value;
+   
+    
+
+    let search = form.search.value; 
+    
+  
+    
+    
+    
+    
     let color = form.color.value;
     let pageNr = 1;
+    
+    
 
     let url = "https://pixabay.com/api/?key=33470155-a1510963a99de7f2888f9d89f&q=" + color + "+" + search + "&per_page=10" + "&page=" + pageNr;
 
     let response = await fetch(url);
     let json = await response.json();
 
+    
+    
 
+   
+
+    
     for (let hit of json.hits) {
 
         let img = document.createElement('img');
@@ -29,9 +45,13 @@ form.onsubmit = async event => {
 
     imageList.append(id, preview);
 
+    
 
     form.search.value = "";
     form.color.value = "";
 
     nextPage = true;
 };
+
+
+    
